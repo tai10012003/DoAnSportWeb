8c92fcde0ee58ac6cfe79f2a9a93f4777ad23768
toan <toandinh.0503@gmail.com>
2025-04-07 15:33:49 +0700

    SCRUM-123 Xử lý validation thông tin nhập vào

--- app/views/cart.php
+++ app/views/cart.php
@@ -19,6 +19,7 @@
 <body>
     <?php include __DIR__ . '/../../includes/header.php'; ?>
 
+    
     <div class="container py-5">
         <div class="row">
             <div class="col-lg-8">
@@ -145,7 +146,7 @@
 
 
 
-    
+
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
--- app/views/get_cart_count.php
+++ app/views/get_cart_count.php
@@ -8,5 +8,6 @@ if (isset($_SESSION['cart'])) {
     }
 }
 
+
 header('Content-Type: application/json');
 echo json_encode(['count' => $count]);

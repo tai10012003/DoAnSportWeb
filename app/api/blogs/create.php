686670880c1100f45c2518213388715d067f9ab8
toan <toandinh.0503@gmail.com>
2025-04-07 17:07:26 +0700

    SCRUM-142 Gọi API xử lý để lấy tất cả bài viết, phản hồi kết quả và hiển thị trên giao diện danh sách bài viết

--- app/api/blogs/create.php
+++ app/api/blogs/create.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json');
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../models/BaiViet.php';
 
+
 try {
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         throw new Exception('Method not allowed');

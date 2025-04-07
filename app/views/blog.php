2d7c3d97f51f16b0eebb103254172e82516e651f
toan <toandinh.0503@gmail.com>
2025-04-07 17:06:41 +0700

    SCRUM-141 Thiết kế các thẻ cho danh sách bài viết

--- app/views/blog.php
+++ app/views/blog.php
@@ -9,6 +9,7 @@ $posts = $data['posts'];
 $totalPages = $data['totalPages'];
 ?>
 
+
 <div class="blog-hero text-center text-white">
     <div class="container">
         <h3 class="display-5 fw-bold mb-3">TIN TỨC & BÀI VIẾT</h3>

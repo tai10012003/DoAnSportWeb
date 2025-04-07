b856669c59ec0ba88d99cdae459d1784e766a005
toan <toandinh.0503@gmail.com>
2025-04-07 14:19:13 +0700

    SCRUM-1102 Kiểm tra hợp lệ dữ liệu người dùng nhập, gọi API xử lý thông tin đăng ký, và phản hồi thông báo

--- app/views/register.php
+++ app/views/register.php
@@ -105,6 +105,8 @@
         </div>
     </div>
 
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
@@ -125,6 +127,10 @@
                 });
             });
         });
+
+
+
+
 
         //xu ly thong tin nguoi dung
         function validateRegisterForm() {
